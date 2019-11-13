8bed489f261cda10d1cda7d2bfb3771face07c7e
Awais Jamil <awais.jamil@containerchain.com>
2019-11-13 16:30:26 +1100

    Modified the Package File for My Library

--- projects/mirza-ionic-forms/src/lib/mirza-ionic-forms.module.ts
+++ projects/mirza-ionic-forms/src/lib/mirza-ionic-forms.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MirzaIonicFormsComponent } from './mirza-ionic-forms.component';
+import {CommonModule} from '@angular/common';
 
 
 
 @NgModule({
   declarations: [MirzaIonicFormsComponent],
   imports: [
+    CommonModule
   ],
   exports: [MirzaIonicFormsComponent]
 })
